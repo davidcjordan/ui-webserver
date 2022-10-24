bb5d994c9da2d689c01217a58d074579a214e9cc
manningt <tom@manningetal.com>
2022-10-24 06:40:18 -0400

    concat logging game/drill when active or paused

--- app/func_base.py
+++ app/func_base.py
@@ -31,6 +31,7 @@ class static_vars:
    previous_base_state = base_state_e.BASE_STATE_NONE            
    faults_table_when_base_not_accessible = None
    previous_base_comms_fault = fault_e.FAULT_BEGIN
+   mode_dict = {}
 
 def check_base():
    # current_app.logger.debug(f'previous_base_state={static_vars.previous_base_state}')
@@ -146,6 +147,7 @@ def check_base():
             current_state =  f"{base_comms_fault.name}"
          except:
             current_app.logger.error(f"base_comms_fault enum to name failed")
+
       if base_state == base_state_e.FAULTED and not base_changed_to_faulted:
          try:
             fault_name = fault_e(faults_table[0][FLT_CODE_PARAM]).name
@@ -153,11 +155,18 @@ def check_base():
             fault_name = ""
          current_state = f"{current_state}: {fault_name}"
 
-      # if base_state == base_state_e.PAUSED or base_state == base_state_e.ACTIVE:
+      if base_state == base_state_e.PAUSED or base_state == base_state_e.ACTIVE:
+         try:
+            mode_name = base_mode_e(static_vars.mode_dict[MODE_PARAM]).name
+         except:
+            mode_name = "unknown"
+         current_state = f"{current_state}: {mode_name}"
+         if static_vars.mode_dict[MODE_PARAM] != base_mode_e.GAME.value:
+            current_state = f"{current_state} ID={static_vars.mode_dict[ID_PARAM]}"
 
-      current_app.logger.info(f"Base state change: {previous_state : <16} -> {current_state}")
+      current_app.logger.info(f"Base state change: {previous_state: <16} -> {current_state}")
       if ops_logger is not None:
-         ops_logger.info(f"Controller state change from {previous_state : <16} to {current_state}")
+         ops_logger.info(f"Controller state change from {previous_state: <16} to {current_state}")
 
    # current_app.logger.debug(f'before copy: new base_state={base_state}  previous={static_vars.previous_base_state}')
    # have to do a copy, otherwise it's a reference to base_state
@@ -210,6 +219,11 @@ def send_start_to_base(mode_dict):
    #  {MODE_PARAM: base_mode_e.GAME.value}
    #  {MODE_PARAM: base_mode_e.CREEP_CALIBRATION.value, ID_PARAM: id}
    
+   try:
+      static_vars.mode_dict = copy.deepcopy(mode_dict)
+   except:
+      current_app.logger.error("copy mode_dict failed")
+
    rc, code = send_msg(PUT_METHOD, MODE_RSRC, mode_dict)
    if not rc:
       current_app.logger.error(f"PUT Mode {mode_dict} failed, code: {code}")
