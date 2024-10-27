982116e5f30ca5ed9eb4828f51a9f987950dc714
manningt <tom@manningetal.com>
2024-10-27 12:42:57 -0400

    first pass at read_drill_csv

--- app/func_drills.py
+++ app/func_drills.py
@@ -3,11 +3,9 @@
 drill class definitions, and read drill files
 '''
 from flask import current_app
+import csv
 
-#TODO: use defines.py:
-drills_dir = "/home/pi/boomer/drills"
-drill_file_prefix = "DRL"
-workout_file_prefix = "WORK"
+from app.main.defines import *
 
 def get_drill_info(drill_id):
    drill_info = {}
@@ -54,3 +52,49 @@ def get_workout_info(workout_id):
    except:
       current_app.logger.error(f"get_drill_info: Error reading '{file_path}'")
    return workout_info
+
+def read_drill_csv(drill_id):
+   drill_info = {}
+
+   if isinstance(drill_id, str):
+      int_drill_id = int(drill_id)
+   elif isinstance(drill_id, int):
+      int_drill_id = drill_id
+   else:
+      current_app.logger.error(f"drill_id {drill_id} is type={type(drill_id)} (not str or int)")
+      return drill_info
+
+   # if int_drill_id >= CUSTOM_DRILL_NUMBER_START and <= CUSTOM_DRILL_NUMBER_END:
+   if int_drill_id >= 400 and int_drill_id <= 499:
+      file_path = f'{settings_dir}/{drill_file_prefix}{int_drill_id:03}.csv'
+   else:
+      file_path = f'{drills_dir}/{drill_file_prefix}{int_drill_id:03}.csv'
+
+   try:
+      # since column titles are the 4th row, need to read them first.
+      with open(file_path) as f:
+         lines = f.read().splitlines()
+         column_names_string = 'column0' + lines[3]
+         current_app.logger.info(f"column_names_string: {column_names_string}")
+         column_names = column_names_string.split(',')
+         # current_app.logger.info(f"column_names: {column_names}")
+   except:
+      current_app.logger.error(f"read_drill_csv: Error reading '{file_path}'")
+
+   throw_list = []
+   if 'SHOT_TYPE' in column_names and 'ROTARY_TYPE' in column_names:
+      try:
+         with open(file_path) as csvfile:
+            reader = csv.DictReader(csvfile, fieldnames=column_names)
+            i = 0
+            for row in reader:
+               if i > 3:
+                  del row['column0']
+                  throw_list.append(row)
+                  # current_app.logger.info(f"row={i} contents={row}")
+               i += 1 
+      except:
+         current_app.logger.error(f"read_drill_csv: Error parsing '{file_path}'")
+   else:
+      current_app.logger.error(f"read_drill_csv: SHOT_TYPE and ROTARY_TYPE not on '{file_path}'")
+   return throw_list
