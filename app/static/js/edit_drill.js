f37eaaf955879e8325d5df1c54b4bac2ec16b2eb
manningt <tom@manningetal.com>
2024-11-04 12:40:14 -0500

    options for add row come from hidden para; add delete row.

--- app/static/js/edit_drill.js
+++ app/static/js/edit_drill.js
@@ -2,23 +2,7 @@ document.getElementById("add_row").addEventListener("click", function (event) {
   event.preventDefault();
   console.log("add_row_button clicked");
   const table = document.getElementById('drill_table');
-
-  // const lastRow = table.rows[table.rows.length - 1];
   const newRowNumber = table.rows.length;
-  // console.log({newRowNumber});
-
-  const cellDict = {};
-  for (let i = 0; i < 7; i++) {
-    cellName = newRowNumber + '-' + i
-    if (i > 0) {
-      cellContent = newRowNumber + '-' + i
-    } else {
-      cellContent = newRowNumber
-    }
-    
-    cellDict[cellName] = cellContent;
-  }
-  // console.log({cellDict});
 
   const row = table.insertRow();
   // first column:
@@ -26,26 +10,26 @@ document.getElementById("add_row").addEventListener("click", function (event) {
   cell.textContent = newRowNumber;
   cell.className = "Cell";
 
-  cell = row.insertCell();
-  cell.innerHTML = '<select name="3-1">\n' +
-      '<option value="Serve">Serve</option>\n' +
-      '<option value="Drop">Drop</option>\n' +
-      '<option value="Flat">Flat</option>\n' +
-      '<option value="Loop">Loop</option>\n' +
-      '<option value="Chip">Chip</option>\n' +
-      '<option value="Lob">Lob</option>\n' +
-      '<option value="Topspin">Topspin</option>\n' +
-      '<option value="Pass">Pass</option>\n' +
-      '<option value="Custom">Custom</option>\n' +
-      '<option value="Rand Grd">Rand Grd</option>\n' +
-      '<option value="Rand Net">Rand Net</option>\n' +
-    '</select>'
+  for (let i = 1; i < 7; i++) {
+    const select_list = document.getElementById('column-' + i); 
+    if (select_list == null) {
+      console.log("select_list is null")
+      // how to handle error ?
+    }
+    //  else {
+    //   console.log(select_list.innerHTML)
+    // }
+    cell = row.insertCell();
+    cell.innerHTML = '<select name="' + newRowNumber + '-' + i +'">\n' + 
+      select_list.innerHTML +
+      '</select>' 
+  }
+}, false);
 
-  // for (const [key, value] of Object.entries(cellDict)) {
-  //   // console.log(key, value);
-  //   const cell = row.insertCell();
-  //   cell.name = key;
-  //   cell.textContent = value;
-  //   cell.className = "Cell";
-  // }
+document.getElementById("del_row").addEventListener("click", function (event) {
+  event.preventDefault();
+  const table = document.getElementById('drill_table');
+  const lastRowNumber = table.rows.length - 1;
+  console.log("del_row_button clicked: delete row=" + lastRowNumber);
+  table.deleteRow(lastRowNumber);
 }, false);
